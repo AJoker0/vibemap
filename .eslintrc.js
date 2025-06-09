827618d4c2414eb1ea34c69852789407f9596b58
Andri Titkov <andrii.titkov@gmail.com>
2025-06-09 06:19:33 +0300

    chore: remove storybook lint rules from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:storybook/recommended"
+    "plugin:prettier/recommended"
   ],
   rules: {
     "prettier/prettier": "error"
